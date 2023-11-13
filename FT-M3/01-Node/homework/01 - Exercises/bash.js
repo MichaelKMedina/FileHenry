0837f29755537280e464d9af07dfb860ce2bad17
Michael Medina Lagos <MMLAGOS@gruposbs.local>
2023-11-13 20:16:20 -0300

    empieza

--- FT-M3/01-Node/homework/01 - Exercises/bash.js
+++ FT-M3/01-Node/homework/01 - Exercises/bash.js
@@ -2,7 +2,17 @@ const process = require('process');
 const { Z_ASCII } = require('zlib');
 const commands = require('./commands/index.js');
 
-function bash() {}
+function bash() {
+   process.stdout.write("prompt > ");
+   process.stdin.on("data", (toString(data)) => {
+      let args;
+   });
+}
+
+function print(output) {
+   process.stdout.write(output)
+   process.stdout.write("\nprompt > ")
+}
 
 bash();
 module.exports = {
