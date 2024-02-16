e00ea8e1faef9d4e7b0038e76cdb6704e6a09fcc
MichaelKMedina <maicollagos@gmail.com>
2024-02-16 15:19:47 -0300

    com

--- Ejercicios/arrays.js
+++ Ejercicios/arrays.js
@@ -1,7 +1,28 @@
-const req = {"url" : "https://rickandmortyapi.com/api/character/1", "otracosa" : "otracosa"}
-const { url } = req;
-const urlMod = url.split('/')
-const id = urlMod.pop();
-console.log(typeof(url));
+const personajes =  [
+    {
+      nombre: "Jon Snow",
+      numeroEpisodio: 5,
+      edad: 23,
+      descripcion: "A bastard son of Ned Stark, joined the Night's Watch.",
+      id: "k7l8m9n0",
+    },
+    {
+      nombre: "Bran Stark",
+      numeroEpisodio: 9,
+      edad: 17,
+      descripcion: "Blessed with supernatural abilities and visions.",
+      id: "i9j0k1l2",
+    },
+  ];
   
+  const deletePersonaje = (personajeID) => {
+    // Tu código aquí
+    const personajeEncontrado = personajes.find((p) => p.id === personajeID);
+    if(!personajeEncontrado) throw Error(`No existe el personaje con id: ${personajeID}`);
+    const hayPersonaje = personajes.filter((p) => p.id !== personajeID);
+    return hayPersonaje;
+  };
+  
+  console.log(deletePersonaje("k7l8m9n0"))
+
 
--- cr-pi-countries-main/server/index.js
+++ cr-pi-countries-main/server/index.js
@@ -1,10 +1,13 @@
 const axios = require("axios");
 const server = require("./src/server");
-const { conn } = require('./src/db.js');
-const PORT = 3001;
+const { conn } = require("./src/db.js");
+const PORT = 5000;
 
-conn.sync({ force: true }).then(() => {
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-})
-}).catch(error => console.error(error))
+conn
+  .sync({ force: true })
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
+  })
+  .catch((error) => console.error(error));
